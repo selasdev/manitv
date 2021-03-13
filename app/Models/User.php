49a1121af4515a40cc33dc61af91f39fc141e3d5
Carlos Rosales <carlosros033@gmail.com>
2021-03-13 12:48:00 -0400

    feat: adding register for common users 🥵

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'username',
+        'firstname',
+        'lastname',
+        'role',
         'email',
         'password',
     ];
