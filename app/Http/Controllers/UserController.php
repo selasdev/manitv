fa5305aaaec37369aa5bf20a834c33ef3fdf02f6
Carlos Rosales <carlosros033@gmail.com>
2021-03-13 14:30:55 -0400

    feat: additional admin user edition views 😨
    -Admin user management module finished

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,16 +5,39 @@ namespace App\Http\Controllers;
 use App\Models\User;
 
 use App\Http\Requests\UserRequest;
+use App\Http\Requests\UserEditRequest;
 use Illuminate\Support\Facades\Hash;
 
 
 class UserController extends Controller
 {
+
     public function index()
+    {
+        if (auth()->user()->role === "admin") {
+            $users = User::latest()->get();
+
+            return view('admin.users.index', compact('users'));
+        } else {
+            return redirect()->route('home');
+        }
+    }
+
+    public function creation()
     {
 
         if (auth()->user()->role === "admin") {
             return view('admin.users.create');
+        } else {
+            return redirect()->route('home');
+        }
+    }
+
+    public function edit(User $user)
+    {
+
+        if (auth()->user()->role === "admin") {
+            return view('admin.users.edit', compact('user'));
         } else {
             return redirect()->route('home');
         }
@@ -33,6 +56,20 @@ class UserController extends Controller
 
         $user->save();
 
-        return back()->with('status', 'Creado con éxito');
+        return back()->with('status', 'Succesfully created');
+    }
+
+    public function update(UserEditRequest $request, User $user)
+    {
+        $user->update([
+            'firstname' => $request['firstname'],
+            'lastname' => $request['lastname'],
+            'username' => $request['username'],
+            'role' => $request['role'],
+        ]);
+
+        $user->save();
+
+        return back()->with('status', 'Succesfully updated');
     }
 }
---
+++ app/Http/Requests/UserEditRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserEditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if (auth()->user()->role === "admin") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'firstname' => ['required', 'string', 'max:255'],
+            'lastname' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:30'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+        ];
+    }
+}
